ec48e0036ae2a454081b529cd9c6b274fbd83192
migloww <miguelruizalzate04@gmail.com>
2024-09-19 07:26:50 -0500

    app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def on_message(client, userdata, message):
 
 broker="broker.mqttdashboard.com"
 port=1883
-client1= paho.Client("GIT-HUB")
+client1= paho.Client("yatchy")
 client1.on_message = on_message
 
 
